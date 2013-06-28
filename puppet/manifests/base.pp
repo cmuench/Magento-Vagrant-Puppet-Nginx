f066aa84956b3b1226fad328add5d19aec8e0edc
Christian Münch <c.muench@netz98.de>
2013-06-28 15:40:45 +0200

    [BUGFIX] Fixed syntax error.

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -2,6 +2,7 @@ node default {
 
     class { "apt":
         always_apt_update => true
+    }
 
     Exec { path => [ "/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/", "/usr/local/bin", "/usr/local/sbin"] }
 
